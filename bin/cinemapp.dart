bed3cdc2dc53f616d101b096b837ae99934000e7
lbarbier22 <lucas.barbier@gastonberger.fr>
2024-01-24 14:29:41 +0100

    feat : first commit

---
+++ bin/cinemapp.dart
@@ -0,0 +1,5 @@
+import 'package:cinemapp/cinemapp.dart' as cinemapp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cinemapp.calculate()}!');
+}
---
+++ test/cinemapp_test.dart
@@ -0,0 +1,8 @@
+import 'package:cinemapp/cinemapp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
